0fb892be0835a4e4cfd1229a194ad6a8da7afa21
Edoardo Nardi <nardiedoardo@gmail.com>
2020-06-01 19:01:35 +0200

    Revert "Revert "Bug Fix""
    This reverts commit 5da6e7e77b6a1c803c9ef6f1fded7aea1e1c1dd5.

--- vast-express/server.js
+++ vast-express/server.js
@@ -6,16 +6,23 @@ var db = new sqlite3.Database('data/vast.db');
 var express = require('express');
 var restapi = express();
 
+restapi.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.append('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+ });
 
-// define a simple entry point to retrieve all users
-restapi.get('/features/hour', function(req, res){
+
+// define a simple entry point to retrieve all features
+restapi.get('/features/hours', function(req, res){
 	db.all("SELECT * from features_h", function(err, rows){
 		res.json(rows);
 	})
 })
 
 
-// get all points of a specified user
+// get all features of a specified sensor
 restapi.get('/features/hour/sensor/:id', function(req, res){
 	var id = req.params.id;
 	if(id){
